e669fa2b681d8bda946447edc4e86e4ead66f433
PetyoKostakov <petyokostakov@gmail.com>
2013-02-16 18:25:33 +0200

    new file

---
+++ N!DivK!.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class NFactDivKFact
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        decimal N = int.Parse(Console.ReadLine());
+        Console.Write("k = ");
+        decimal K = int.Parse(Console.ReadLine());
+
+        decimal factorialN = 1;
+        do
+        {
+            factorialN *= N;
+            N--;
+        }
+        while (N > 0);
+
+        decimal factorialK = 1;
+
+        do
+        {
+            factorialK *= K;
+            K--;
+        }
+        while (K > 0);
+
+        decimal res = factorialN / factorialK;
+
+        Console.WriteLine("n! = " + factorialN);
+        Console.WriteLine("k! = " + factorialK);
+        Console.WriteLine("n!/k! = " + res);
+    }
+}
+
